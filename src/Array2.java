caa5994fb8b3a516be67f7d83b9ddc8e6440c8fc
Petr <petrkovtun1@gmail.com>
2024-03-15 00:28:38 +0200

    Initial Commit

---
+++ src/Array2.java
@@ -0,0 +1,6 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31,30, 31, 30, 31};
+        System.out.println(" В апреле " + monthDays[3] + "  дней.");
+    }
+}
