e02301301f25c81e22f91f9cf91617b354b0b7e5
Petr <petrkovtun1@gmail.com>
2024-03-15 16:32:37 +0200

    Добавил одну строку печати

--- src/Array2.java
+++ src/Array2.java
@@ -2,5 +2,7 @@ public class Array2 {
     public static void main(String[] args) {
         int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31,30, 31, 30, 31};
         System.out.println(" В апреле " + monthDays[3] + "  дней.");
-    }
+        System.out.println(" В мае " + monthDays[4] + " дней.");
+        
+}
 }
